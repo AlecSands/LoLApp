c5f0cda2e24a9cb705dd17a9b7cc72c292404a52
AlecSands <sandsalecv@gmail.com>
2017-04-23 14:22:38 -0500

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,5 +1,9 @@
+var summonerInfo;
+
+function preload() {
+    summonerInfo = loadJSON("summonerinfo.json");
+}
+
 function main() {
-    $.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/sand0848?api_key=RGAPI-1f6ceaad-7c6d-4b9d-8624-c25b883afa41", function(data){
-  alert("Data: " + data);
-});
+    alert(summonerInfo);
 }
