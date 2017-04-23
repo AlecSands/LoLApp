5d984c702a63a203b9a6563de4cb6f653fb09ed2
AlecSands <sandsalecv@gmail.com>
2017-04-23 13:24:11 -0500

    query test

--- jstest.js
+++ jstest.js
@@ -1,3 +1,5 @@
 function main() {
-  $('.champlist' 
+    $.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/sand0848?api_key=RGAPI-1f6ceaad-7c6d-4b9d-8624-c25b883afa41", function(data){
+  alert("Data: " + data);
+});
 }
